ac6bc6cc7833824555654db7f51c08e58fb03812
Zeynab Jibreel <zeina2025@gmail.com>
2020-06-12 23:28:57 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,18 +1,19 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
   new_hash = {}
-  data.each do |main, values|
-    hash.each do |value, array|
+  data.each do |property, hash|
+    hash.each do |attribute, array|
       array.each do |name|
         if !new_hash.has_key?(name)
           new_hash[name] = {}
         end
 
-        if !new_hash[name].has_key?(main)
-          new_hash[name][main] = []
+        if !new_hash[name].has_key?(property)
+          new_hash[name][property] = []
         end
-        if !new_hash[name][main].include?(value)
-          new_hash[name][value] << value.to_s
+
+        if !new_hash[name][property].include?(attribute)
+          new_hash[name][property] << attribute.to_s
         end
       end
     end
